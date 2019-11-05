a8644d33536d257455dbb5dd6f3c904ee84c0414
A29sTech <A29.Connect@gmail.com>
2019-11-05 18:16:09 +0530

    Some Changes Done.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-let tm = document.getElementById('name');
+const showContact = ()=>{
+    let tm = document.getElementById('about');
+    tm.style.display = "block";
+    tm.className = tm.className + " slideInUp"
+}
+
+document.onscroll = () => {
+    showContact();
+    document.onscroll = null;
+};
+document.onclick = ()=>{
+    showContact();
+    document.onclick = null;
+};
+document.onwheel = ()=>{
+    showContact();
+    document.onmouseover = null;
+}
